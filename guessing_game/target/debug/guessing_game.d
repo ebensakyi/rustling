f45e012098fa19bf428b05c7cf3ed027efcb9af8
Kofi <ebensakyi@gmail.com>
2022-04-19 16:53:21 +0000

    rustling chapter 2

--- guessing_game/target/debug/guessing_game.d
+++ guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/Volumes/DATA/PROJECTS/RUST/guessing_game/target/debug/guessing_game: /Volumes/DATA/PROJECTS/RUST/guessing_game/src/main.rs
+D:\PROJECTS\MINE\rustling\guessing_game\target\debug\guessing_game.exe: D:\PROJECTS\MINE\rustling\guessing_game\src\main.rs
