21e3bf2a0e98b8a0cbc5ec0e1d496b52e54cf4eb
Kofi Agyemang <ebensakyi@gmail.com>
2022-04-26 21:10:36 +0000

    Data types

--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -4,7 +4,6 @@ fn main() {
 
     // subtraction
     let difference = 95.5 - 4.3;
-
     // multiplication
     let product = 4 * 30;
 
@@ -23,4 +22,13 @@ fn main() {
     let (x, y, z) = tup;
 
     println!("The value of y is: {}", y);
+
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
 }
